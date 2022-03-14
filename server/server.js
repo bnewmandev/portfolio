3e881c491604fc2ec586e01086c8ecb29aa6dad3
Ben Newman <ben@bndev.co.uk>
2022-03-14 19:23:27 +0000

    removed contact form, replaced with email

--- server/server.js
+++ server/server.js
@@ -1,11 +1,25 @@
 const express = require("express");
+const bodyParser = require("body-parser")
+
 const port = 8080;
 const app = express();
 
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}))
+
 app.get("/ping", (req, res) => {
     res.send("pong!")
-})
+});
+
+// app.post("/contact", (req, res) => {
+//     console.log(req.body)
+//     // console.log(req.body.contactemail)
+//     // console.log(req.body.contactphone)
+//     // console.log(req.body.contactphone)
+//     res.redirect("/#success")
+// })
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`)
-})
+});
+
