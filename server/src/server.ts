5e1173a35207ef0f72afa14a11f6b20c152f9fd6
Ben Newman <ben@bndev.co.uk>
2022-03-19 12:06:23 +0000

    converted homepage to ejs

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,11 +4,17 @@ import bodyParser from "body-parser";
 const port = 8080;
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/ping", (req, res) => {
   res.send("pong!");
+});
+
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
 app.listen(port, () => {
