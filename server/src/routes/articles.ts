20439a39ea60e31484eec12ff074197bcddcda32
Ben Newman <ben@bndev.co.uk>
2022-03-21 17:08:20 +0000

    worked on ejs for article preview

--- server/src/routes/articles.ts
+++ server/src/routes/articles.ts
@@ -2,8 +2,36 @@ import express from "express";
 
 const router = express.Router();
 
+const articles = [
+  {
+    title: "Test Article 1",
+    createdAt: 1647695614248,
+    description: "test description",
+  },
+  {
+    title: "Test Article 2",
+    createdAt: 1647695614248,
+    description: "test description",
+  },
+  {
+    title: "Test Article 3",
+    createdAt: 1647695614248,
+    description: "test description",
+  },
+  {
+    title: "Test Article 3",
+    createdAt: 1647695614248,
+    description: "test description",
+  },
+  {
+    title: "Test Article 3",
+    createdAt: 1647695614248,
+    description: "test description",
+  },
+];
+
 router.get("/", (req, res) => {
-  res.render("blog");
+  res.render("blog", { articles });
 });
 
 export = router;
