2ba0dfbcf1edf79e02d569e3815c238a6dd7faa9
Ben Newman <ben@bndev.co.uk>
2022-05-10 20:31:26 +0100

    refactored project modals to seperate ejs files from index

--- server/src/routes/root.ts
+++ server/src/routes/root.ts
@@ -1,10 +1,12 @@
 import express from "express";
-import bcrypt from "bcrypt";
+import data from "../../portfolioData.json";
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("index");
+  console.log(data);
+  res.render("index", {items: data});
+
 });
 
 export = router;
